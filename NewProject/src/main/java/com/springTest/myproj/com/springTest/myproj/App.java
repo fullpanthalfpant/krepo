4322bc2ba6e53eda656a7d69d668d95d6f88d5a1
kaustubh <KS@10.0.0.16>
2020-07-07 01:05:52 -0400

    commiting the changes

---
+++ NewProject/src/main/java/com/springTest/myproj/com/springTest/myproj/App.java
@@ -0,0 +1,13 @@
+package com.springTest.myproj.com.springTest.myproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
